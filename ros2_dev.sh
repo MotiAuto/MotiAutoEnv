e93c68791b8439215f2f2bf90f587ec83e13f17f
motii <108280115+motii8128@users.noreply.github.com>
2025-01-05 14:32:03 +0900

    Update ros2_dev.sh

--- ros2_dev.sh
+++ ros2_dev.sh
@@ -12,21 +12,23 @@ locale
 sudo apt install -y software-properties-common
 sudo add-apt-repository universe
 
-echo "Add ROS2 GPG key"
+echo "----------Add ROS2 GPG key----------"
 
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-echo "Install ROS2 development tools"
+echo "----------Install ROS2 development tools----------"
 sudo apt update && sudo apt install -y ros-dev-tools
 
-echo "Install ROS2"
+echo "----------Install ROS2----------"
 sudo apt update
 sudo apt upgrade
 sudo apt install -y ros-jazzy-desktop
 
 source /opt/ros/jazzy/setup.bash
+
+echo "----------Install vcstool, nodejs, npm and ros-webbridge----------"
 
 sudo apt install python3-vcstool -y
 sudo apt install nodejs npm -y
